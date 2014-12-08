18bb3e6dceb62b781d3b59c0aa9b64fae553237b
Scott Maroney <scott.maroney@gmail.com>
2014-12-07 19:24:24 -0800

    added minimal capability for point tracking

--- packages/cvtls2/public/controllers/players.js
+++ packages/cvtls2/public/controllers/players.js
@@ -1,61 +1,27 @@
 'use strict';
 
-angular.module('mean').controller('PlayersController', ['$scope', '$stateParams', '$location', 'Global', 'Players',
-    function($scope, $stateParams, $location, Global, Players) {
-        $scope.global = Global;
+angular.module('mean').controller('PlayersController', ['$scope', '$stateParams', '$location', 'Global', 'Players','Matches', 
+	function($scope, $stateParams, $location, Global, Players, Matches) {
+		$scope.global = Global;
 
-        $scope.create = function() {
-            var player = new Players({
-                title: this.title,
-                content: this.content
-            });
-            player.$save(function(response) {
-                $location.path('players/' + response._id);
-            });
+		$scope.find = function() {
+			Players.query(function(players) {
+			$scope.players= players;
+			for( var ii=0; ii < $scope.players.length; ii=ii+1) {
+				$scope.players[ii].points = 0;
+				$scope.players[ii].ranking = 2;
+				if($scope.players[ii].firstName === 'Doug') {
+					$scope.players[ii].points = 3;
+					$scope.players[ii].ranking = 1;
+				}
+			}
+			});
+		};
 
-            this.title = '';
-            this.content = '';
-        };
-
-        $scope.remove = function(player) {
-            if (player) {
-                player.$remove();
-
-                for (var i in $scope.players) {
-                    if ($scope.players[i] === player) {
-                        $scope.players.splice(i, 1);
-                    }
-                }
-            } else {
-                $scope.player.$remove();
-                $location.path('players');
-            }
-        };
-
-        $scope.update = function() {
-            var player = $scope.player;
-            if (!player.updated) {
-                player.updated = [];
-            }
-            player.updated.push(new Date().getTime());
-
-            player.$update(function() {
-                $location.path('players/' + player._id);
-            });
-        };
-
-        $scope.find = function() {
-            Players.query(function(players) {
-                $scope.players= players;
-            });
-        };
-
-        $scope.findOne = function() {
-            Players.get({
-                playerId: $stateParams.playerId
-            }, function(player) {
-                $scope.player = player;
-            });
-        };
-    }
+		$scope.findOne = function() {
+			Players.get({ playerId: $stateParams.playerId }, function(player) {
+				$scope.player = player;
+			});
+		};
+	}
 ]);
